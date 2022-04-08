ab25d33f413b449597f145d6a877b7d7a165031c
gnavink <34332355+gnavink@users.noreply.github.com>
2022-04-08 10:33:54 +0530

    Delete Day2 directory

--- Day2/Functions/area_triangle.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def area_triangle(a, b, c):
-    s = (a + b + c) / 2
-    return math.sqrt(s * (s - a) * (s - b) * (s - c))
-
-#area = area_triangle(3,4,5)  
-area = area_triangle(3,4)
-print(area)
