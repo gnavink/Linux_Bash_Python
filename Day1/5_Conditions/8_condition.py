a9782ca91b01f5078a1995b1f2f9487e09189c91
gnavink <34332355+gnavink@users.noreply.github.com>
2022-04-08 10:33:40 +0530

    Delete Day1 directory

--- Day1/5_Conditions/8_condition.py
+++
@@ -1,7 +0,0 @@
-for n in range(3):
-    password = input("Password: ")
-    if password == "I<3Bieber":
-        break
-    print("Password is incorrect.")
-else:
-    print("Suspicious activity. The authorities have been alerted.")
