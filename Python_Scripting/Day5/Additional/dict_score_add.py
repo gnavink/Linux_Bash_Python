fb3b550aef2cbab689893385c24de3b1db2e7142
gnavink <34332355+gnavink@users.noreply.github.com>
2022-04-10 16:51:28 +0530

    Delete Python_Scripting/Day5 directory

--- Python_Scripting/Day5/Additional/dict_score_add.py
+++
@@ -1,6 +0,0 @@
-for n in ["Dhawan","Kohli"]: 
-    total[n] = 0 
-    for match in score.keys(): 
-        if n in score[match].keys(): 
-           total[n] = total[n] + score[match][n]
-
--- Python_Scripting/Day5/Additional/power.py
+++
@@ -1,8 +0,0 @@
-def power(x,n):
-    ans = 1 
-    for i in range(0,n): 
-        ans = ans*x 
-    return(ans)
-    
-
-power(n=5, x=4)
